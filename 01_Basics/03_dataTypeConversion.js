853a02345bd1b644ac4ae3dad5986b6cc5e5be56
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-13 14:04:06 +0000

    conversion of datatypes

---
+++ 01_Basics/03_dataTypeConversion.js
@@ -0,0 +1,50 @@
+let marks = "7outOf10"
+
+console.log(typeof marks);
+console.log(typeof (marks));
+
+let valeInNumber = Number(marks)
+console.log(typeof valeInNumber);
+
+console.log(valeInNumber)
+
+
+// "7" => 7
+// "7outOf10" => NaN
+//  true => 1;
+// false => 0;
+
+
+let needToPass = "harshit"
+
+let youArePassed = Boolean(needToPass)
+console.log(youArePassed)
+
+harshit => true
+
+//**************** oprations **************** 
+
+let num = 2
+let negNum = -num
+console.log(-num);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+console.log(2/2);
+
+
+console.log("1" + 2); 
+console.log(1 + "2");
+console.log("1" + 2 + 2); 
+console.log(1 + 2 + "2"); 
+console.log(1 + "2" + 2); 
+
+console.log(+true);
+console.log(+"");
+
+let scoreCounter = 100;
+scoreCounter++;
+console.log(scoreCounter);
