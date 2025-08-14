55af9ec763f305bc8948bb84a1d563d1fca43512
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-14 17:30:47 +0000

    Numbers and Math

---
+++ 01_Basics/06_numAndMaths.js
@@ -0,0 +1,29 @@
+const finishes = 200
+console.log(finishes)
+
+const score = new Number(100)
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const otherNum = 123.3232
+console.log(otherNum.toPrecision(4));
+
+//******************** Maths *******************/
+
+console.log(Math.abs(-9)); //convert negative- value to positive+
+console.log(Math.round(5.5)); // to round of the value
+console.log(Math.ceil(8.1)); //to  get the higher value
+console.log(Math.floor(8.1)); //to get the lower value
+console.log(Math.min(7, 8, 7, 8, 4, 48, 81,)); // to find the minimum value
+console.log(Math.max(7, 8, 7, 8, 4, 48, 81,)); // to find the maximum value
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) +  min);
