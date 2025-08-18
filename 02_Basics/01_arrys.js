73f3459fae0327ed46e475939a95e47b668d267c
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-18 17:40:58 +0000

    arrays

---
+++ 02_Basics/01_arrys.js
@@ -0,0 +1,50 @@
+// array
+
+const newArr = [1, 2, 3, 4, 5, 6]
+const dcHeroes = ["Batman", "Superman", "Flash"]
+
+console.log(dcHeroes[1]);
+
+
+// Array Methodes
+
+console.log(newArr);
+
+newArr.push(7) // To add/push values at the end 0f array
+console.log(newArr);
+
+newArr.pop() // To  remove the last value of array
+console.log(newArr);
+
+
+// newArr.shift()// To  remove the first value of array
+// console.log(newArr);
+
+
+newArr.unshift(9)// To add/push values at the start 0f array
+console.log(newArr);
+
+
+
+
+// console.log(newArr.includes(5)); // To find if the value exist in the array of not
+// console.log(newArr.indexOf(9)); // To find the index of value
+
+// const myArr = newArr.join();
+
+// console.log(myArr);
+// console.log(typeof(myArr));
+
+
+// Slice and Splice
+
+// console.log("A", newArr);
+
+// const newArr2 = newArr.slice(2, 5)
+
+// console.log(newArr2);
+// console.log("B", newArr);
+
+// const newArr3 = newArr.splice(0, 6)
+// console.log("C", newArr);
+// console.log(newArr3);
