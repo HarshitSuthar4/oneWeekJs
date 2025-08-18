77550c053c75a5341331d0402437e9509a4a54a2
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-18 14:09:42 +0000

    coverd date and time in depth

---
+++ 01_Basics/07_dateAndTime.js
@@ -0,0 +1,13 @@
+// let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate); // Date is considered as an object 
+
+let createNewDate = new Date("18-8-2025")
+console.log(createNewDate);
+console.log(createNewDate.toLocaleString());
+
