76481bb251fd1b6889987c3461a08f74c66cbc4d
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-19 13:33:54 +0000

    obj. destructuring

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,11 @@
+const product = {
+    productName: "Gaming Chair",
+    isFunctional: "yes",
+    price: 12999
+}
+
+// product.productName 
+
+const {isFunctional: functional} = product // deStructuering
+ 
+console.log(functional); 
