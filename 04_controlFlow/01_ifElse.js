ef9f68bf8039e4dea9a525a22dceb0fe7a683e94
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-22 17:42:39 +0000

    if and else condition

---
+++ 04_controlFlow/01_ifElse.js
@@ -0,0 +1,45 @@
+// const temp = 41;
+// if (temp === 42) {
+//     console.log("less than 40");
+// } else {
+//     console.log("temp is greter than 40");
+// }
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const price = 199
+
+// if (price > 150){
+//     let product = "controller"
+//     console.log(`Product name: ${product}`);
+// }
+// console.log(`Product name: ${product}`); // this line wont be executed bcause Variable Product is not in scope.
+
+const balance = 780;
+// if (balance > 500) console.log("more than 500"), console.log(""); // Not a good practice avoide useing this syntax or structue
+
+// if (balance < 500) {
+//     console.log("Less Than 500");
+// } else if (balance < 680) {
+//     console.log("Less than 680");
+
+// } else if (balance <= 890) {
+//     console.log("Less than 890");
+
+// } else {
+//     console.log("More than 2900");
+
+// } // Basic if and else and how does it work
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard ){
+//     console.log("Eligible for shoping");
+// } // If statment useing && oprator
+
+// if (loggedInFromEmail || loggedInFromGoogle){
+//     console.log("User logged in");   
+// } // If statment useing || oprator
