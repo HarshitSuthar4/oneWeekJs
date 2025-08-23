21099c243e58450edee162683aa8808642cf91c0
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-23 19:03:33 +0000

    for loop

---
+++ 05_iterators/03_forLoop.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for (const element of arr) {
+    console.log(element);
+}
+
+const welcomeMessage = "Hello World"
+
+for (const element of welcomeMessage) {
+    console.log(element);  
+}
