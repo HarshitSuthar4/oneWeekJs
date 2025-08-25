33e9eb45f810234582f985a39c968f3da903b9a5
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-25 17:07:03 +0000

    for of and for in loops

--- 05_iterators/03_forOfLoop.js
+++ 05_iterators/03_forOfLoop.js
@@ -1,11 +1,26 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
 for (const element of arr) {
-    console.log(element);
+    // console.log(element);
 }
 
 const welcomeMessage = "Hello World"
 
 for (const element of welcomeMessage) {
-    console.log(element);  
+    // console.log(element);  
 }
+
+const programmingLang = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+}
+
+for (const key in programmingLang) {
+    // console.log(`${key} shourts is for ${programmingLang[key]}` );
+} // for in loop
+
+const lang = ["js", "cpp", "java", "python"]
+for (const key in lang) {
+    // console.log(lang[key]);
+} // for in with arry
