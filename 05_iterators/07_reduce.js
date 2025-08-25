e57676d747eaaafaf2577857f866585821df1db1
HarshitSuthar4 <harshitsuthar482@gmail.com>
2025-08-25 18:43:01 +0000

    reduce

--- 05_iterators/07_reduce.js
+++ 05_iterators/07_reduce.js
@@ -1,12 +1,11 @@
-const numbers = [1, 8, 5, 7, 6]
+const numbers = [1, 2, 3, 4]
 
 // const totalNum = numbers.reduce(function (acc, curr) {
 //     console.log(`acc: ${acc} and curr: ${curr}`);
 //     return acc + curr
-// }, 2) // reduce using a normal function
-
+// }, 0) // reduce using a normal function
 // console.log(totalNum);
 
-const totalNum = numbers.reduce((acc, curr) => { return acc + curr}, 0)
+const totalNum = numbers.reduce((acc, curr) => {  return acc + curr}, 0)
 
 console.log(totalNum);
